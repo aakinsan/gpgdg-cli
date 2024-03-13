035c0d564a2088f1ca6f3a52866386c107610b31
adeola <adeola.akinsanya@gmail.com>
2024-03-13 15:35:18 -0400

    modified logging handler

--- clients/logger_client.py
+++ clients/logger_client.py
@@ -2,30 +2,27 @@
 Logging Module 
     - logging.
 """
-
 import logging
 import google.cloud.logging
-from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
+from google.cloud.logging.handlers import CloudLoggingHandler
 
 
 # Instantiates a Google Cloud Logging Client
 client = google.cloud.logging.Client()
 
-# Create Log handler.
+# Retrieves a Cloud Logging handler
 cloud_handler = CloudLoggingHandler(client)
 
 # Create formatter and set for the handler.
 formatter = logging.Formatter("%(asctime)s : %(name)s : %(levelname)s : %(message)s")
+
 cloud_handler.setFormatter(formatter)
 
 # Use Python’s standard logging library to send logs to GCP
-gpg_logger = logging.getLogger("gpg_logger")
+gpg_logger = logging.getLogger('gpgdg_cli')
 
-# Set minimum level of logger to info.
+# Set minimum level of logger to info
 gpg_logger.setLevel(logging.INFO)
 
-setup_logging(cloud_handler)
-
-# Add handler to logger.
 gpg_logger.addHandler(cloud_handler)
 
