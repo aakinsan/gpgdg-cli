530c17270f58b6ea84d1238119bb042ec8f1229f
adeola <adeola.akinsanya@gmail.com>
2024-03-13 16:37:21 -0400

    updated logging handler

--- clients/logger_client.py
+++ clients/logger_client.py
@@ -1,20 +1,32 @@
 """
 Logging Module 
-    - logging.
+    - logging to stdout and Google Logging API.
 """
 import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
 
-gpg_logger = logging.getLogger('gpgdg_cli')
+# Instantiates a Google Cloud Logging Client.
+client = google.cloud.logging.Client()
 
+# Setup handler to make GCP logging API calls.
+cloud_handler = CloudLoggingHandler(client)
+
+# Setup handler to stream to stdout.
 handler = logging.StreamHandler()
 
-# Create formatter and set for the handler.
+# Create log formatter and set for the handlers.
 formatter = logging.Formatter("%(asctime)s : %(name)s : %(levelname)s : %(message)s")
-
+cloud_handler.setFormatter(formatter)
 handler.setFormatter(formatter)
 
-gpg_logger.addHandler(handler)
+# Setup logger to create log Records and disable ancestor loggers.
+gpg_logger = logging.getLogger('gpgdg_cli')
+gpg_logger.propagate = False
 
+# Set minimum logging level to info.
 gpg_logger.setLevel(logging.INFO)
 
-handler.setFormatter(formatter)
+# Attach handlers to the logger.
+setup_logging(cloud_handler)
+gpg_logger.addHandler(handler)
